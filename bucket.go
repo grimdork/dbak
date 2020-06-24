5a4c9844acc537e5b3cfc2179da0c369cbf57273
Ronny Bangsund <github@neuralshortcircuit.com>
2020-06-24 17:28:38 +0200

    S3 upload implemented.

--- bucket.go
+++ bucket.go
@@ -1,29 +1,52 @@
 package main
 
 import (
+	"os"
+	"path/filepath"
+
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/cheggaaa/pb/v3"
 )
 
 // Bucket in S3 or compatible system.
 type Bucket struct {
+	sess     *session.Session
 	srv      *s3.S3
 	Name     string
 	contents []*s3.Object
 }
 
 // NewBucket sets up a new bucket for access.
-func NewBucket(srv *s3.S3, name string) *Bucket {
+func NewBucket(region, name string) (*Bucket, error) {
+	awscfg := &aws.Config{
+		Region: aws.String(region),
+	}
+	// Use shared credentials in ~/.aws/credentials or from envvars.
+	// Set AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY.
+	sess, err := session.NewSession(awscfg)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = sess.Config.Credentials.Get()
+	if err != nil {
+		return nil, err
+	}
+
 	b := &Bucket{
-		srv:  srv,
+		sess: sess,
+		srv:  s3.New(sess),
 		Name: name,
 	}
-	return b
+
+	return b, nil
 }
 
 // List contents of bucket.
 func (b *Bucket) List() ([]*s3.Object, error) {
-	// b.srv.ListObjects()
 	res, err := b.srv.ListObjectsV2(&s3.ListObjectsV2Input{Bucket: aws.String(b.Name)})
 	if err != nil {
 		return nil, err
@@ -31,4 +54,35 @@ func (b *Bucket) List() ([]*s3.Object, error) {
 
 	b.contents = res.Contents
 	return b.contents, nil
+}
+
+// Upload a file to S3 with progress.
+func (b *Bucket) Upload(fn string) error {
+	f, err := os.Open(fn)
+	if err != nil {
+		return err
+	}
+
+	defer f.Close()
+	fi, err := f.Stat()
+	if err != nil {
+		return err
+	}
+
+	bar := pb.Full.Start64(fi.Size())
+	defer bar.Finish()
+	r := bar.NewProxyReader(f)
+	name := filepath.Join("db", filepath.Base(fn))
+	pr("Uploading to %s", name)
+	uploader := s3manager.NewUploader(b.sess)
+	_, err = uploader.Upload(&s3manager.UploadInput{
+		Bucket: aws.String(b.Name),
+		Key:    aws.String(name),
+		Body:   r,
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
